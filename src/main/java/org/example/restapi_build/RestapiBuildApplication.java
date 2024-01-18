cd6b367a958453e36cc8ab8487b1c7de19b51e06
Park Sung Jun <junstin119@gmail.com>
2024-01-18 23:39:45 +0900

    first commit

---
+++ src/main/java/org/example/restapi_build/RestapiBuildApplication.java
@@ -0,0 +1,13 @@
+package org.example.restapi_build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiBuildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/restapi_build/RestapiBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restapi_build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiBuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
