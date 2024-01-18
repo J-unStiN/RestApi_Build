1f2d2bbf46ace789e3cac88b4ca10165b71cbfd3
Park Sung Jun <junstin119@gmail.com>
2024-01-18 23:45:17 +0900

    Update RestapiBuildApplicationTests.java
    - 첫 테스트코드 추가

--- src/test/java/org/example/restapi_build/RestapiBuildApplicationTests.java
+++ src/test/java/org/example/restapi_build/RestapiBuildApplicationTests.java
@@ -1,13 +1,16 @@
 package org.example.restapi_build;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class RestapiBuildApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertThat(42).isEqualTo(42);
     }
 
 }
